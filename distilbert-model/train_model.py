f40cec8652dc3bb749e621851b6b285451498c75
dillonchong01 <dillonchong01@gmail.com>
2025-04-29 11:28:14 +0800

    Updated

---
+++ distilbert-model/train_model.py
@@ -0,0 +1,69 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from sklearn.model_selection import train_test_split
+import torch
+from datasets import Dataset
+from intent_train_data import data
+from sklearn.metrics import accuracy_score, f1_score
+
+
+# Split Data into Train/Test
+texts, labels = zip(*data)
+train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels, test_size=0.2, random_state=42)
+
+# Tokenize
+tokenizer = AutoTokenizer.from_pretrained("huawei-noah/TinyBERT_General_4L_312D")
+train_encodings = tokenizer(list(train_texts), truncation=True, padding=True)
+test_encodings = tokenizer(list(test_texts), truncation=True, padding=True)
+
+# Create HuggingFace Dataset
+train_dataset = Dataset.from_dict({
+    'input_ids': train_encodings['input_ids'],
+    'attention_mask': train_encodings['attention_mask'],
+    'labels': train_labels
+})
+val_dataset = Dataset.from_dict({
+    'input_ids': test_encodings['input_ids'],
+    'attention_mask': test_encodings['attention_mask'],
+    'labels': test_labels
+})
+
+# Train Model
+model = AutoModelForSequenceClassification.from_pretrained("huawei-noah/TinyBERT_General_4L_312D", num_labels=4)
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    num_train_epochs=10,
+    weight_decay=0.01,
+    logging_dir="./logs",
+    logging_steps=10,
+    save_strategy="epoch",
+)
+
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    acc = accuracy_score(labels, preds)
+    f1 = f1_score(labels, preds, average='weighted')
+    return {
+        'accuracy': acc,
+        'f1': f1,
+    }
+
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    compute_metrics=compute_metrics, 
+)
+
+trainer.train()
+
+# Save the Model
+model.save_pretrained("./tinybert_model")
+tokenizer.save_pretrained("./tinybert_model")
